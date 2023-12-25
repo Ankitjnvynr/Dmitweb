39c2525f2f03a0768556e5f5605d1456e8dea515
ankitjnvynr <ankitbkana@outlook.com>
2023-12-25 11:14:51 +0530

    fgfgd

--- partials/_db.php
+++ partials/_db.php
@@ -4,6 +4,11 @@ $username = "root";
 $password = "";
 $db = "dmit";
 
+$servername = "localhost";
+$username = "u133609271_dmit";
+$password = "Dmit@12345";
+$db = "u133609271_dmit";
+
 // Create connection
 $conn = new mysqli($servername, $username, $password,$db);
 
